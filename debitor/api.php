2ff49426530e9c1c1727a399f361c7396a2c60ec
sirrolin <nicolaieno@msn.com>
2026-05-04 10:55:35 +0300

    Finalised  Batch Bilags Matching

--- debitor/api.php
+++ debitor/api.php
@@ -106,7 +106,7 @@
         
         $timestamp = date("Y-m-d-H-i-s");
         
-        if ($response === false || isset($response["error"]) || isset($response["errorNumber"]) || $response === null || $response === "") {
+        if ($response === false || isset($response["error"]) || isset($response["errorNumber"]) || $response === null || trim($response) === "") {
             // An error occurred
             $errorNumber = curl_errno($ch);
             $errorMessage = curl_error($ch);
@@ -115,7 +115,7 @@
             // Save error response in temp folder
             file_put_contents("../temp/$db/Update-company-error-$timestamp.json", json_encode($error, JSON_UNESCAPED_UNICODE)."\n".json_encode($data, JSON_UNESCAPED_UNICODE));
             
-            return ['success' => false, 'message' => 'Error updating company: ' . $errorMessage];
+            return ['success' => false, 'message' => 'Error updating company: ' . json_encode($errorMessage, JSON_PRETTY_PRINT)];
         }
         
         // Save successful response in temp folder for debugging
